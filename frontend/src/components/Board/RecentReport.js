54422aec25776a0479cb944fcf41cd987f953458
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2023-06-19 21:04:24 +0530

    update the board and recent report

--- frontend/src/components/Board/RecentReport.js
+++ frontend/src/components/Board/RecentReport.js
@@ -1,25 +1,25 @@
 
-// import React, { useState } from "react";
-// var id;
-// function RecentReport() {
-//   const [report, setReport] = useState("");
-//   //getReport
-//   fetch({
-//     url: `http://localhost:8000/api/build/${id}`,
-//     method: "GET",
-//     mode: "no-cors",
-//   })
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((data) => {
-//       var result = data.result;
-//       console.log(result);
-//       setReport(result);
-//     });
+import React, { useState } from "react";
+var id;
+function RecentReport() {
+  const [report, setReport] = useState("");
+  //getReport
+  fetch({
+    url: `http://localhost:8000/api/build/${id}`,
+    method: "GET",
+    mode: "no-cors",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      var result = data.result;
+      console.log(result);
+      setReport(result);
+    });
 
-//   console.log(report);
-//   return <div>RecentReport</div>;
-// }
+  console.log(report);
+  return <div>RecentReport</div>;
+}
 
-// export default RecentReport;
+export default RecentReport;
